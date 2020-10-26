ecbc357d884128563c6b9bbb2f3ffc99ea0c1dcb
kirillo15 <kirillof84@gmail.com>
2020-10-26 22:19:24 +0300

    html

--- excel-course/webpack.config.js
+++ excel-course/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -8,22 +8,23 @@ const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
 const jsLoaders = () => {
   const loaders = [
-      {
-        loader: 'babel-loader',
+    {
+      loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env']
       }
     }
   ]
+
   if (isDev) {
     loaders.push('eslint-loader')
   }
+
   return loaders
-
-} 
-
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -54,12 +55,17 @@ module.exports = {
         collapseWhitespace: isProd
       }
     }),
-    new CopyPlugin([
-      {
+    new CopyPlugin({
+
+      patterns: [{
+    
         from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
+    
+         to: path.resolve(__dirname, 'dist')
+    
+      }]
+    
+    }),
     new MiniCssExtractPlugin({
       filename: filename('css')
     })
